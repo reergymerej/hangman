a3107831f3257920a32adc50f10b91b8478b4d15
Jeremy Greer <jeremy.greer.atl@gmail.com>
2017-05-12 20:44:29 -0500

    Initial Commit

---
+++ hangman/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  hangman
+//
+//  Created by Jeremy Greer on 5/12/17.
+//  Copyright © 2017 Grizzle. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
